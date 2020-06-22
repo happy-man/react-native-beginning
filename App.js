46cf915b23f61c7ad2a90287fd09d91776104eb5
Rostyslav Siliveistr <rosilivejster@gmail.com>
2020-06-22 16:34:04 +0300

    Got familiar with ListView component

--- App.js
+++ App.js
@@ -1,49 +1,39 @@
-import React, {useState} from 'react';
-import {Image, ScrollView, Text} from 'react-native';
+import React from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 
-const logo = {
-  uri: 'https://reactnative.dev/img/tiny_logo.png',
-  width: 64,
-  height: 64,
-};
-
-export default function PizzaTranslator() {
+export default function FlatListBasics() {
   return (
-    <ScrollView
-      contentContainerStyle={{
-        alignItems: 'flex-end',
-      }}>
-      <Text style={{fontSize: 96}}>Scroll me plz</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 96}}>If you like</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 96}}>Scrolling down</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 96}}>What's the best</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 96}}>Framework around?</Text>
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Image source={logo} />
-      <Text style={{fontSize: 80}}>React Native</Text>
-    </ScrollView>
+    <View style={styles.container}>
+      <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dan'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={obj => {
+          console.log(obj); // direct keys: item, index, separators
+          return <Text style={styles.item}>{obj.item.key}</Text>;
+        }}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});

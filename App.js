1b939b1a7823aba0736cda8cc8a46e53bd92c2ce
Rostyslav Siliveistr <rosilivejster@gmail.com>
2020-06-23 17:16:16 +0300

    Added state to React Native components

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import {View, ScrollView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-export default function FlatListBasics() {
+export default function App() {
+  const [name, setName] = useState('Rost');
+  const [person, setPerson] = useState({name: 'Johnny', age: 28});
+
+  const clickHandler = () => {
+    setName('Bobby');
+    setPerson({name: 'Zeus', age: 100});
+  };
+
   return (
-    <View style={{flex: 1}}>
-      <View style={{backgroundColor: 'powderblue', flex: 1}} />
-      <View style={{backgroundColor: 'skyblue', flex: 2}} />
-      <View style={{backgroundColor: 'steelblue', flex: 3, maxHeight: 100}} />
+    <View style={styles.container}>
+      <Text>My name is {name}</Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update name" onPress={clickHandler} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+});
